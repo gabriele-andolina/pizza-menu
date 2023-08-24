1d4591bb38c3d21cbc9ff6b112e31d4e14b3d67f
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-24 13:58:50 +0200

    Created Header component

--- src/index.js
+++ src/index.js
@@ -49,12 +49,16 @@ const pizzaData = [
 function App() {
     return (
         <div>
-            <h1>Hello, React!</h1>
+            <Header />
             <Pizza />
             <Pizza />
             <Pizza />
         </div>
     );
+}
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>;
 }
 
 function Pizza() {
