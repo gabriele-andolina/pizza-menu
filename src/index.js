4aad3a84ca73f8e0546e5cafd95111ba49ad2b60
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-24 15:49:50 +0200

    Added className attribute to app components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -57,23 +58,39 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React Pizza Co.</h1>;
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>;
+        </header>
+    );
 }
 
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
-        </div>
+        </main>
     );
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openingHour = 12;
+    const closingHour = 22;
+    const isOpen = hour >= openingHour && hour <= closingHour;
+    console.log(isOpen);
+
+    // if (hour >= openingHour && hour <= closingHour)
+    //     alert("We're currently open!");
+    // else alert("Sorry, we're closed!");
+
     return (
-        <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+        <footer className="footer">
+            {new Date().toLocaleTimeString()} We're currently open!
+        </footer>
     );
 }
 
@@ -81,7 +98,7 @@ function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     );
