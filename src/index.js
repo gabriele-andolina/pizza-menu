c993c187658a2a5b08bf94a3098fbcc7558f8d14
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-29 14:19:22 +0200

    Rendered menu conditionally if numOfPizzas > 0

--- src/index.js
+++ src/index.js
@@ -66,15 +66,20 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = pizzaData;
+    const numOfPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our menu</h2>
 
-            <ul className="pizzas">
-                {pizzaData.map((pizza) => (
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
-                ))}
-            </ul>
+            {numOfPizzas > 0 && (
+                <ul className="pizzas">
+                    {pizzas.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            )}
 
             {/* <Pizza
                 name="Pizza Spinaci"
