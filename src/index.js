f91693688aabdf0d45aac7d65217b78d93c28948
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-29 16:03:58 +0200

    Used ternary operator in Menu and Footer to render data conditionally

--- src/index.js
+++ src/index.js
@@ -73,12 +73,17 @@ function Menu() {
         <main className="menu">
             <h2>Our menu</h2>
 
-            {numOfPizzas > 0 && (
+            {numOfPizzas > 0 ? (
                 <ul className="pizzas">
                     {pizzas.map((pizza) => (
                         <Pizza pizzaObj={pizza} key={pizza.name} />
                     ))}
                 </ul>
+            ) : (
+                <p>
+                    We're still working on our menu. Please come back at a later
+                    time.{" "}
+                </p>
             )}
 
             {/* <Pizza
@@ -125,7 +130,7 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
                 <div className="order">
                     <p>
                         We're open until {closingHour}:00. Come visit us or
@@ -133,6 +138,11 @@ function Footer() {
                     </p>
                     <button className="btn">Order</button>
                 </div>
+            ) : (
+                <p>
+                    We're happy to welcome you between {openingHour}:00 and{" "}
+                    {closingHour}:00
+                </p>
             )}
         </footer>
     );
