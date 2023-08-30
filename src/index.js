a8a42f629128f0be9d21f65fd1a14eab5e15523d
gabriele-andolina <gabe.andolina@gmail.com>
2023-08-30 12:31:42 +0200

    Wrapped <p> and <ul> in fragment in Menu

--- src/index.js
+++ src/index.js
@@ -74,11 +74,18 @@ function Menu() {
             <h2>Our menu</h2>
 
             {numOfPizzas > 0 ? (
-                <ul className="pizzas">
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian cuisine. Six creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (
                 <p>
                     We're still working on our menu. Please come back at a later
